ebdd5c78b1c7ac8f7c97c8f35839933f5e9fde88
unfv_alexwill@hotmail.com <prueba@correo.com>
2024-07-07 02:42:22 -0500

    Refactorizando recurso Weather

--- JwtAuthAspNet8WebAPI/Controllers/WeatherForecastController.cs
+++ JwtAuthAspNet8WebAPI/Controllers/WeatherForecastController.cs
@@ -11,23 +11,18 @@ namespace JwtAuthAspNet8WebAPI.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        [HttpGet]
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        [Route("Get")]
+    
+        public IActionResult Get() 
         {
-            _logger = logger;
+        
+           return Ok(Summaries);
+        
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
+
+
     }
 }
